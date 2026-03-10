4bf4e9b83042a4a0fe9f80504363d188633bc328
Egor Pozdnov <pozdner98@gmail.com>
2026-03-11 00:00:02 +0300

    test

--- src/test/java/org/example/TestTest.java
+++ src/test/java/org/example/TestTest.java
@@ -3,11 +3,13 @@ package org.example;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 public class TestTest {
 
     @Test
     void test(){
-        System.out.println("true");
+        assertThat(2).isEqualTo(1+1);
     }
 }
